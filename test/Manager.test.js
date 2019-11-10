09116928e4f8f28e74e02c28a6c39e5fa069a107
Chris Neal <voodoodaddy2000@gmail.com>
2019-11-10 04:13:40 -0700

    Added methods and passes testing

--- test/Manager.test.js
+++ test/Manager.test.js
@@ -1,5 +1,5 @@
-const Manager = require("../lib/Manager");
-const Employee = require("../lib/Employee");
+const Manager = require("../lib/constructors/Manager");
+const Employee = require("../lib/constructors/Employee");
 
 test("Can set office number via constructor argument", () => {
   const testValue = 100;
