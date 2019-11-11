cb0e8e45a8808ee949d4588c88af4e7af9f204b8
Chris Neal <voodoodaddy2000@gmail.com>
2019-11-11 15:29:01 -0700

    Added validation to the questions

--- index.js
+++ index.js
@@ -18,21 +18,48 @@ const managerQuestions = [
         type: "input",
         name: "name",
         message: "What is the manager's name?",
+        validate: answer => {
+            if (answer || answer.trim().length) {
+                return true;
+            }
+            return "Please enter the manager's name";
+        }
     },
     {
         type: "input",
         name: "id",
         message: "What is the manager's id number?",
+        validate: answer => {
+            if (typeof id !== "number" || isNaN(id)) {
+                return "Please enter a valid number";
+            }
+            return true;
+        }
     },
     {
         type: "input",
         name: "email",
         message: "What is the manager's email?",
+        validate: answer => {
+            const pass = answer.match(
+              /\S+@\S+\.\S+/
+            );
+            if (pass) {
+              return true;
+            }
+            return "Please enter a valid email address.";
+          }
     },
     {
         type: "input",
         name: "officeNumber",
         message: "What is the manager's office number",
+        validate: answer => {
+            if (typeof id !== "number" || isNaN(id)) {
+                return "Please enter a valid number";
+            }
+            return true;
+        }
     },
 ];
 const engineerQuestions = [
@@ -40,21 +67,48 @@ const engineerQuestions = [
         type: "input",
         name: "name",
         message: "What is the engineers's name?",
+        validate: answer => {
+            if (answer || answer.trim().length) {
+                return true;
+            }
+            return "Please enter the engineer's name";
+        }
     },
     {
         type: "input",
         name: "id",
         message: "What is the engineers's id number?",
+        validate: answer => {
+            if (typeof id !== "number" || isNaN(id)) {
+                return "Please enter a valid number";
+            }
+            return true;
+        }
     },
     {
         type: "input",
         name: "email",
         message: "What is the engineers's email?",
+        validate: answer => {
+            const pass = answer.match(
+              /\S+@\S+\.\S+/
+            );
+            if (pass) {
+              return true;
+            }
+            return "Please enter a valid email address.";
+          }
     },
     {
         type: "input",
         name: "github",
         message: "What is the engineers's GitHub username?",
+        validate: answer => {
+            if (answer || answer.trim().length) {
+                return true;
+            }
+            return "Please enter the engineer's GitHub username";
+        }
     },
 ];
 const internQuestions = [
@@ -62,21 +116,48 @@ const internQuestions = [
         type: "input",
         name: "name",
         message: "What is the intern's name?",
+        validate: answer => {
+            if (answer || answer.trim().length) {
+                return true;
+            }
+            return "Please enter the intern's name";
+        }
     },
     {
         type: "input",
         name: "id",
         message: "What is the intern's id number?",
+        validate: answer => {
+            if (typeof id !== "number" || isNaN(id)) {
+                return "Please enter a valid number";
+            }
+            return true;
+        }
     },
     {
         type: "input",
         name: "email",
         message: "What is the intern's email?",
+        validate: answer => {
+            const pass = answer.match(
+              /\S+@\S+\.\S+/
+            );
+            if (pass) {
+              return true;
+            }
+            return "Please enter a valid email address.";
+          }
     },
     {
         type: "input",
         name: "school",
         message: "What is the name of the intern's school?",
+        validate: answer => {
+            if (answer || answer.trim().length) {
+                return true;
+            }
+            return "Please enter the intern's school name";
+        }
     },
 ];
 
