5b4947bac43a67d653d99614d29ce92965dcd90a
Chris Neal <voodoodaddy2000@gmail.com>
2019-11-11 16:43:18 -0700

    Added README text and commented code

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # 08_Team_Profile_Generator
+
+### Table of Contents
+- [Description](#Description)
+- [Technologies-Libraries](#Technologies-Libraries)
+- [Screenshots](#Screenshots)
+- [Link](#Link)
+​
+### Description
+This assignment is a software engineering team generator command line application. The application prompts the user for information about the team manager and then information about the team members. The user can input any number of team members, and they may be a mix of engineers and interns. This assignment also passes a jest unit tests. When the user has completed building the team, the application creates an HTML file that displays a formatted team roster based on the information provided by the user. 
+
+### Technologies-Libraries
+- [NodeJS](https://nodejs.dev/) - Server side code
+- [inquirer](https://www.npmjs.com/search?q=inquirer) - User inputs on the command line
+​
+### Screenshots
+​
+![Image](assets/images/run.gif)
+​
+### Link
+Check it out! 
+https://github.com/chrisneal72/08_Team_Profile_Generator
+https://chrisneal72.github.io/08_Team_Profile_Generator/output/
--- index.js
+++ index.js
@@ -3,186 +3,47 @@ const inquirer = require("inquirer");
 
 // Local Modules
 const render = require("./lib/htmlrenderer");
+const questions = require("./lib/questions"); //MOVED QUESTIONS TO THEIR OWN FILE TO MAKE THIS FILE CLEANER
 
 // Constructors
 const Manager = require("./lib/constructors/Manager");
 const Engineer = require("./lib/constructors/Engineer");
 const Intern = require("./lib/constructors/Intern");
 
-
-
 // Global Variables
+//ARRAY OF TEAM MEMBERS
 const teamMembers = [];
-const managerQuestions = [
-    {
-        type: "input",
-        name: "name",
-        message: "What is the manager's name?",
-        validate: answer => {
-            if (answer || answer.trim().length) {
-                return true;
-            }
-            return "Please enter the manager's name";
-        }
-    },
-    {
-        type: "input",
-        name: "id",
-        message: "What is the manager's id number?",
-        validate: answer => {
-            if (typeof id !== "number" || isNaN(id)) {
-                return "Please enter a valid number";
-            }
-            return true;
-        }
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "What is the manager's email?",
-        validate: answer => {
-            const pass = answer.match(
-              /\S+@\S+\.\S+/
-            );
-            if (pass) {
-              return true;
-            }
-            return "Please enter a valid email address.";
-          }
-    },
-    {
-        type: "input",
-        name: "officeNumber",
-        message: "What is the manager's office number",
-        validate: answer => {
-            if (typeof id !== "number" || isNaN(id)) {
-                return "Please enter a valid number";
-            }
-            return true;
-        }
-    },
-];
-const engineerQuestions = [
-    {
-        type: "input",
-        name: "name",
-        message: "What is the engineers's name?",
-        validate: answer => {
-            if (answer || answer.trim().length) {
-                return true;
-            }
-            return "Please enter the engineer's name";
-        }
-    },
-    {
-        type: "input",
-        name: "id",
-        message: "What is the engineers's id number?",
-        validate: answer => {
-            if (typeof id !== "number" || isNaN(id)) {
-                return "Please enter a valid number";
-            }
-            return true;
-        }
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "What is the engineers's email?",
-        validate: answer => {
-            const pass = answer.match(
-              /\S+@\S+\.\S+/
-            );
-            if (pass) {
-              return true;
-            }
-            return "Please enter a valid email address.";
-          }
-    },
-    {
-        type: "input",
-        name: "github",
-        message: "What is the engineers's GitHub username?",
-        validate: answer => {
-            if (answer || answer.trim().length) {
-                return true;
-            }
-            return "Please enter the engineer's GitHub username";
-        }
-    },
-];
-const internQuestions = [
-    {
-        type: "input",
-        name: "name",
-        message: "What is the intern's name?",
-        validate: answer => {
-            if (answer || answer.trim().length) {
-                return true;
-            }
-            return "Please enter the intern's name";
-        }
-    },
-    {
-        type: "input",
-        name: "id",
-        message: "What is the intern's id number?",
-        validate: answer => {
-            if (typeof id !== "number" || isNaN(id)) {
-                return "Please enter a valid number";
-            }
-            return true;
-        }
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "What is the intern's email?",
-        validate: answer => {
-            const pass = answer.match(
-              /\S+@\S+\.\S+/
-            );
-            if (pass) {
-              return true;
-            }
-            return "Please enter a valid email address.";
-          }
-    },
-    {
-        type: "input",
-        name: "school",
-        message: "What is the name of the intern's school?",
-        validate: answer => {
-            if (answer || answer.trim().length) {
-                return true;
-            }
-            return "Please enter the intern's school name";
-        }
-    },
-];
 
 async function init() {
     try {
-        const response = await inquirer.prompt(managerQuestions);
+        //START BY ASKING THE MANAGER QUESTIONS
+        const response = await inquirer.prompt(questions.managerQuestions);
+        //ADD THE MANAGER OBJECT TO THE ARRAY
         teamMembers.push(new Manager(response.name, response.id, response.email, response.officeNumber));
+        //RUN A WHILE LOOP TO GET THE TEAM MEMBERS
+        //moreTeam TRACKS IF THE USER WANTS TO ADD ANOTHER MEMEBER
         let moreTeam = true;
         while (moreTeam) {
+            //THIS QUESTION ASKS FOR WHAT TYP OF MEMBER SO WE KNOW WHAT QUESTIONS TO ASK
             const memberType = await inquirer.prompt({
                 type: "list",
                 message: "Select team member type:",
                 name: "memberType",
                 choices: ["Engineer", "Intern", "No other team members to add"]
             });
+            //ASK ENG OR INTERN QUESTION AND ADD ITS OBJECT TO THE ARRAY
             if (memberType.memberType === "Engineer") {
-                const res = await inquirer.prompt(engineerQuestions);
+                const res = await inquirer.prompt(questions.engineerQuestions);
                 teamMembers.push(new Engineer(res.name, res.id, res.email, res.github));
             } else if (memberType.memberType === "Intern") {
-                const res = await inquirer.prompt(internQuestions);
+                const res = await inquirer.prompt(questions.internQuestions);
                 teamMembers.push(new Intern(res.name, res.id, res.email, res.school));
             } else {
+                //IF THEY ARE DONE ADDING THEN FLIP THE VAR AND END THE WHILE
                 moreTeam = !moreTeam;
             }
         }
+        //GO INTO htmlrenderer.js TO BUILD THE OBJECTS AND THE HTML
         render(teamMembers);
     } catch (err) {
         console.log(err);
